a248d6055db48a9c0da250861bf3226760c04cc3
Ray Cook <rayscook@gmail.com>
2023-10-01 23:30:08 -0400

    made changes to mobile styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,43 +7,50 @@ interface HeaderProps {
 }
 const Header: React.FC<HeaderProps> = ({ isSubscriber }) => {
   return (
-    <div className="bg-black p-4 text-white flex justify-between items-center">
-      <div className="text-2xl font-bold flex w-1/3">
-        <img src={ftfeedLogo} alt="FT Feed Logo" className="w-10 h-10 mr-2" />
-        Friend Tech Feed
+    <div className="bg-black p-4 text-white flex flex-col sm:flex-row justify-between items-center">
+      <div className="text-2xl font-bold flex flex-col sm:flex-row w-full sm:w-1/3 items-center">
+        {/* Logo and Brand Name */}
+        <div className="flex items-center mb-4 sm:mb-0">
+          <img src={ftfeedLogo} alt="FT Feed Logo" className="w-10 h-10 mr-2" />
+          Friend Tech Feed
+        </div>
+        {/* By Rciv section */}
         <a
           href="https://www.friend.tech/rooms/0x85f8c70a0ab0c948a3ed0236e2cc245719ae084c"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex items-center mb-8 sm:mb-0 sm:ml-20"
         >
-          <div className="flex">
-            <img
-              src="https://pbs.twimg.com/profile_images/1658158682541109254/Mv2DnQ_1_400x400.jpg"
-              alt="RCIV"
-              className="w-10 h-10 mr-2 rounded-full ml-20"
-            />
-            By Rciv
-          </div>
+          <img
+            src="https://pbs.twimg.com/profile_images/1658158682541109254/Mv2DnQ_1_400x400.jpg"
+            alt="RCIV"
+            className="w-10 h-10 mr-2 rounded-full"
+          />
+          By Rciv
         </a>
       </div>
       {isSubscriber ? (
-        <div className="text-sm text-gray-400">
-          <span className="text-[#c4ff0e] bg-gray-700 rounded-lg p-4 font-bold">
+        <div className="text-sm text-gray-400 ">
+          <span className="text-[#c4ff0e] bg-gray-700 rounded-lg sm:p-4 font-bold">
             Thank you for owning an RCIV family key
           </span>
         </div>
       ) : (
         <div className="text-sm text-gray-400">
-          <span className="text-[#c4ff0e] bg-gray-700 rounded-lg p-4 font-bold">
-            <a href="https://www.friend.tech/trades/0x85f8c70a0ab0c948a3ed0236e2cc245719ae084c"
-            target = "_blank"
-            rel = "noopener noreferrer"
-            >Purchase my key or one of my holder's keys to access real time data.</a>
+          <span className="text-[#c4ff0e] bg-gray-700 rounded-lg sm:p-4 font-bold">
+            <a
+              href="https://www.friend.tech/trades/0x85f8c70a0ab0c948a3ed0236e2cc245719ae084c"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Purchase my key or one of my holder's keys to access real time
+              data.
+            </a>
           </span>
         </div>
       )}
 
-      <div>
+      <div className="mt-8 sm:mt-0">
         <ConnectButton />
       </div>
     </div>
